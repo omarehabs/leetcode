cf2aa5e1be32dc152067260f0f9a9c7d09a19953
omarehab <omarehabsal@gmail.com>
2023-01-28 08:33:34 +0200

    improve valid-parentheses code

--- validParentheses.js
+++ validParentheses.js
@@ -1,23 +1,12 @@
 var isValid = function (s) {
-  s = Array.from(s.replace(/\s+/g, ''));
-  const strs = {
-    '(': ')',
-    '{': '}',
-    '[': ']',
-  };
-
-  let valid = false;
-
+  s = s.replace(/\s+/g, '');
+  const strs = { '(': ')', '{': '}', '[': ']' };
   const arr = [];
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === strs[arr[arr.length - 1]]) {
-      arr.pop();
-    } else {
-      arr.push(s[i]);
-    }
-  }
 
-  arr.length === 0 ? (valid = true) : (valid = false);
-  return valid;
+  for (let i = 0; i < s.length; i++)
+    s[i] === strs[arr[arr.length - 1]] ? arr.pop() : arr.push(s[i]);
+
+  return !arr.length;
 };
-console.log(isValid(' () [ {}] [[{}]] []'));
+
+console.log(isValid(' () [ {{}}] [[{}]] []'));
