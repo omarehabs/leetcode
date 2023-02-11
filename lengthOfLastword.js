43d525a0bdddca6b4b9c4d360109877158ea99f0
omarehab <omarehabsal@gmail.com>
2023-02-11 22:50:43 +0200

    update .gitignore file

--- lengthOfLastword.js
+++
@@ -1,6 +0,0 @@
-const lengthOfLastWord = function (s) {
-  const words = s.trim().split(' ');
-  return words[words.length - 1].length;
-};
-
-console.log(lengthOfLastWord('LeetCode problems are not easy   '));
